a7e65f5b5ae08d7ff361ee8cd08f7ce048fa0bd0
Graalstrike <f.forquignon@sudalys.fr>
2026-02-16 14:33:13 +0100

    fix: filename to match recipe

--- DependencyInjection/GraalstrikeJasperReportExtension.php
+++ DependencyInjection/GraalstrikeJasperReportExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class JasperReportExtension extends Extension
+class GraalstrikeJasperReportExtension extends Extension
 {
     /**
      * {@inheritdoc}
